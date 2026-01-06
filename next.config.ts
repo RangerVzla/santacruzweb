b5c7d143f2da5a3fb763e69826adf78acd7cf81f
RangerVzla <yanluis.laya@gmail.com>
2026-01-06 19:27:21 -0300

    Actualizar configuraciones y agregar headers

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',        // genera HTML estático en /out
+  images: { unoptimized: true } // desactiva Image Optimization (incompatible con 'export')
 };
 
 export default nextConfig;
+
